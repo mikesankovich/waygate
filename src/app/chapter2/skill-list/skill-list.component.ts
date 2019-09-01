494f895a240f399c3cfe4da5cd205adeab8d2a1f
Michael Sankovich <misankovich@gmail.com>
2019-09-01 12:19:09 -0400

    filter skills

--- src/app/chapter2/skill-list/skill-list.component.ts
+++ src/app/chapter2/skill-list/skill-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { skills } from './skills.json';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-skill-list',
@@ -8,8 +9,31 @@ import { skills } from './skills.json';
 })
 export class SkillListComponent implements OnInit {
   skills = [];
+  searchTerm = '';
   constructor() {
-    this.skills = skills;
+    this.skills = _.cloneDeep(skills);
+    this.filteredSkills = _.cloneDeep(skills);
+  }
+
+  onChange(e) {
+    this.searchTerm = e.target.value;
+  }
+
+  onFilter() {
+    const term = this.searchTerm;
+    console.log(term);
+    if (!term) {
+      this.filteredSkills = this.skills;
+    } else {
+      this.filteredSkills = this.skills.filter(skill => {
+        const str = JSON.stringify(skill).toLowerCase();
+        return str.indexOf(term.toLowerCase()) > -1;
+      });
+    }
+  }
+
+  clearFilter() {
+    this.filteredSkills = this.skills;
   }
 
   ngOnInit() {
