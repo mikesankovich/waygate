bdc10eb45705b5d0cf753d39f355566210f7182a
Michael Sankovich <misankovich@gmail.com>
2019-09-14 10:22:44 -0400

    more shifting

--- src/app/chapter1/spend-skill-points/spend-skill-points.component.ts
+++ src/app/chapter1/spend-skill-points/spend-skill-points.component.ts
@@ -1,18 +1,21 @@
 import { Component, ElementRef, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import chapterContent from '../contents/spend-skill-points.json';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-spend-skill-points',
   templateUrl: './spend-skill-points.component.html',
   styleUrls: ['./spend-skill-points.component.scss'],
-  // encapsulation: ViewEncapsulation.None,
 })
 export class SpendSkillPointsComponent {
   chapter: string = '';
-  contents = chapterContent;
-  constructor(private route:ActivatedRoute, private elem: ElementRef) {
-    this.chapter = route.snapshot.data.chapter;
+  contents;
+  constructor(private api: ApiService) {
+    api.get('content', 'chapter1', 'spend-skill-points').subscribe(e => {
+      this.contents = e;
+    }, (error) => {
+      console.log(error);
+    });
   }
 
 }
